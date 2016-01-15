24953d3ffd1b104dc94e54f82ccaab4481e6282c
feng.zhang <cst.feng@gmail.com>
2016-01-15 12:46:31 +0900

    convert java to kotlin

---
+++ app/src/androidTest/java/foryou/learnkotlin/ApplicationTest.kt
@@ -0,0 +1,9 @@
+package foryou.learnkotlin
+
+import android.app.Application
+import android.test.ApplicationTestCase
+
+/**
+ * [Testing Fundamentals](http://d.android.com/tools/testing/testing_android.html)
+ */
+class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
